fb893dbcac81bc2ecfaa9c88aa284be5e1f44739
Fausio Matsinhe <fausio.matsinhe@kooli.io>
2023-03-22 09:41:49 +0200

    first commit

---
+++ src/test/java/fausio/domain/employeemanager/EmployeemanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package fausio.domain.employeemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeemanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
