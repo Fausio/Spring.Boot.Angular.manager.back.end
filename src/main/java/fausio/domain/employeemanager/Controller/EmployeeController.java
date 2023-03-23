02eea0434aec08722cd492034f37c077c6a136df
Fausio Matsinhe <fausio.matsinhe@kooli.io>
2023-03-23 09:06:43 +0200

    clean

--- src/main/java/fausio/domain/employeemanager/Controller/EmployeeController.java
+++ src/main/java/fausio/domain/employeemanager/Controller/EmployeeController.java
@@ -3,8 +3,7 @@ package fausio.domain.employeemanager.Controller;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.http.ResponseEntity; 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
