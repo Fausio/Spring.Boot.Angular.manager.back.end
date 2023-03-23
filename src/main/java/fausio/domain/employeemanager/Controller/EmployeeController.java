eda076041e5fe36569e540a15146d50ee5951ee5
Fausio Matsinhe <fausio.matsinhe@kooli.io>
2023-03-23 09:05:10 +0200

    CorsFilter corsFilter

--- src/main/java/fausio/domain/employeemanager/Controller/EmployeeController.java
+++ src/main/java/fausio/domain/employeemanager/Controller/EmployeeController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import fausio.domain.employeemanager.model.Employee;
 import fausio.domain.employeemanager.service.EmployeeService;
 
+@CrossOrigin(origins = "http://localhost:4200/") 
 @RestController
 @RequestMapping("/employee")
 public class EmployeeController {
